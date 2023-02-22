e80e944fa60508704c7a1e7a925e0fa01865b8ae
Alonso Bonilla <luis.labp06@gmail.com>
2023-02-21 22:53:56 -0500

    Selectors

---
+++ js/modules/selectors.js
@@ -0,0 +1,5 @@
+const result = document.querySelector('#result');
+const form = document.querySelector('#form');
+const paginationDiv = document.querySelector('#pagination');
+
+export { result, form, paginationDiv};
