ca5d7dd1f0ac03c72c427c4f59249a0fa0e939a9
Alonso Bonilla <luis.labp06@gmail.com>
2023-03-22 17:49:47 -0500

    Passing to async await

--- js/modules/fetch.js
+++ js/modules/fetch.js
@@ -3,18 +3,19 @@ import { currentPage } from "./functions.js";
 
 const resultsByPage = 40;
 
-function getImages(term) {
-    console.log(currentPage);
+async function getImages(term) {
     const key = '33829705-d86a2ac4485ff315a80e9ab75';
     const url = `https://pixabay.com/api/?key=${key}&q=yellow+flowers&q=${term}&per_page=${resultsByPage}&page=${currentPage}`;
     
-    console.log(currentPage);
-    fetch(url)
-        .then(answer => answer.json())
-        .then(result => {
-            calculatePages(result.totalHits, resultsByPage);
-            showImages(result.hits);
-        })
+    try {
+        const answer = await fetch(url);
+        const result = await answer.json();
+
+        calculatePages(result.totalHits, resultsByPage);
+        showImages(result.hits);
+    } catch(error) {
+        console.log(error);
+    }
 }
 
 export { getImages };
